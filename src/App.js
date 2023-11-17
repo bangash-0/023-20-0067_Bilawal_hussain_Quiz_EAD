f92ab27735c0a3757cfd5f40d858f8f9d7377301
Bilawal Hussain <117905020+bangash-0@users.noreply.github.com>
2023-11-17 15:00:42 +0500

    Hero Section done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import Offerings from "./components/Offerings";
 
 function App() {
 	return (
 		<div className="App">
 			<NavBar />
 			<Hero />
+			<Offerings />
 			<Footer />
 		</div>
 	);
