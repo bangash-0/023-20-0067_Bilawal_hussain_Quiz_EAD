71723f9cd48d350a55befce573af4e077c20dfd7
Bilawal Hussain <117905020+bangash-0@users.noreply.github.com>
2023-11-17 12:37:12 +0500

    structure added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import "./App.css";
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Header />
+			<Hero />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
